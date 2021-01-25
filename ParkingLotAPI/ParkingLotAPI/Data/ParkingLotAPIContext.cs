3ac318a9fbedb1f9bf743116ca41567d6077c0b9
João Oliveira <joaoamoliveira8@upskill.pt>
2021-01-25 12:28:29 +0000

    ParkingLotAPI created

---
+++ ParkingLotAPI/ParkingLotAPI/Data/ParkingLotAPIContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ParkingLotAPI.Models;
+
+namespace ParkingLotAPI.Data
+{
+    public class ParkingLotAPIContext : DbContext
+    {
+        public ParkingLotAPIContext (DbContextOptions<ParkingLotAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ParkingLotAPI.Models.Slot> Slot { get; set; }
+    }
+}
---
+++ ParkingLotAPI/ParkingLotAPI/Models/Slot.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkingLotAPI.Models
+{
+    public class Slot
+    {
+        public int Id { get; set; }
+        public int Place { get; set; }
+        public string Row { get; set; }
+        public int Floor { get; set; }
+        public string Availability { get; set; }
+        public double Price { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public bool IsEletricChargeAvailable { get; set; } = false;
+
+        public string CoordinatesToString()
+        {
+            return String.Format("{0},{1}", this.Latitude, this.Longitude);
+        }
+    }
+}
