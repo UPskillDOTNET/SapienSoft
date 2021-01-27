e352a6bef11742f08f16136b8d9d7a08773a9817
Alexandre Alves <afelixalves@upskill.pt>
2021-01-27 15:30:40 +0000

    Nova versão Park2API

---
+++ iPark/Park2API/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Park2API.Models;
+
+namespace Park2API.Contexts
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+    }
+}
---
+++ iPark/Park2API/Services/IUserService.cs
@@ -0,0 +1,17 @@
+﻿using Park2API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Park2API.Services
+{
+    public interface IUserService
+    {
+        Task<string> RegisterAsync(RegisterModel model);
+
+        Task<AuthenticationModel> GetTokenAsync(TokenRequestModel model);
+
+        Task<string> AddRoleAsync(AddRoleModel model);
+    }
+}
