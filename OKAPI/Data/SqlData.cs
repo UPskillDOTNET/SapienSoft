f2b75e47460cbad6acf4cf63d668e22ce1041abc
jorge-mrh <you@example.com>
2021-01-28 10:12:38 +0000

    agora sim, segue a pasta

---
+++ OKAPI/Data/SqlData.cs
@@ -0,0 +1,56 @@
+﻿using OKAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OKAPI.Data
+{
+
+    public class SqlData : IData
+    {
+        private Context _context;
+        public SqlData(Context context)
+        {
+            _context = context;
+        }
+
+        public Slot AddSlot(Slot slot)
+        {
+            slot.Id = Guid.NewGuid();
+            _context.Slots.Add(slot);
+            _context.SaveChanges();
+            return slot;
+        }
+
+        public void DeleteSlot(Slot slot)
+        {
+            _context.Slots.Remove(slot);
+            _context.SaveChanges();
+        }
+
+        public Slot EditSlot(Slot slot)
+        {
+            var existingSlot = _context.Slots.Find(slot.Id);
+            if (existingSlot != null) 
+            {
+                existingSlot.Status = slot.Status;
+                _context.Slots.Update(existingSlot);
+                _context.SaveChanges();
+            }
+            return slot;
+        }
+
+        public Slot GetSlot(Guid id)
+        {
+            var slot = _context.Slots.Find(id);
+
+            return slot;
+        }
+
+        public List<Slot> GetSlots()
+        {
+            return _context.Slots.ToList();
+        }
+    }
+}
