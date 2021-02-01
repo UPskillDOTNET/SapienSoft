471cb0f5d2e77253eb87f735a3d786ea4a7e255a
jorge-mrh <you@example.com>
2021-02-01 13:12:00 +0000

    User Authorization and Authentication, User Roles, JWT

---
+++ iPark/iParkCentralAPI/Services/IUserService.cs
@@ -0,0 +1,24 @@
+﻿using iParkCentralAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using iParkCentralAPI.Services;
+using Authorization = iParkCentralAPI.Constants.Authorization;
+
+
+namespace iParkCentralAPI.Services
+{
+    public interface IUserService
+    {
+        Task<string> RegisterAsync(RegisterModel model);
+
+        Task<AuthenticationModel> GetTokenAsync(TokenRequestModel model);
+
+        Task<string> AddRoleAsync(AddRoleModel model);
+
+    }
+
+
+}
