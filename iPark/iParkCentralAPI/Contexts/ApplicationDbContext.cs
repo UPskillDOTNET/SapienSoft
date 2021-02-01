9c040f89d32effe69b69157e6e4a395c31939ad1
João Oliveira <joaoamoliveira8@upskill.pt>
2021-02-01 17:17:07 +0000

    bug fixes

--- iPark/iParkCentralAPI/Contexts/ApplicationDbContext.cs
+++ iPark/iParkCentralAPI/Contexts/ApplicationDbContext.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using iParkCentralAPI.Entities;
 
 namespace iParkCentralAPI.Contexts
 {
@@ -14,5 +15,16 @@ namespace iParkCentralAPI.Contexts
             {
 
             }
+        public DbSet<iParkCentralAPI.Entities.Reservation> Reservation { get; set; }
+        public DbSet<iParkCentralAPI.Entities.Park> Park { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.HasDefaultSchema("Identity");
+            builder.Entity<Park>().HasData(
+                new Park { Id=1, Name = "Park 1", OwnerContact = "cmp@cm-porto.pt", IsChargingAvailable = true, BaseUrl = "http://localhost:49161/" },
+                new Park { Id=2, Name = "Park 2", OwnerContact = "portic@ipp.pt", IsChargingAvailable = false, BaseUrl = "http://localhost:49162/", Latitude = 41.17652121446711, Longitude = -8.60566234613385, IsCovered = false }
+                );
+        }
     }
 }
