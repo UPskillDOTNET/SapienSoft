8180b6fe2e0b33fdf7bc2905af3c6272f3a47c03
Alexandre Alves <afelixalves@upskill.pt>
2021-02-05 11:45:17 +0000

    create services for parks

---
+++ iParkPro/iParkPro/Controllers/ReservationsController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using iParkPro.Contexts;
+using iParkPro.Entities;
+using iParkPro.Services;
+
+namespace iParkPro.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReservationsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IParkService _parkService;
+
+        public ReservationsController(ApplicationDbContext context, IParkService parkService)
+        {
+            _context = context;
+            _parkService = parkService;
+        }
+
+
+        // GET: api/Reservations
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Reservation>>> GetReservations()
+        {
+            return await _context.Reservations.ToListAsync();
+        }
+
+        // GET: api/Reservations/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Reservation>> GetReservation(int id)
+        {
+            var reservation = await _context.Reservations.FindAsync(id);
+
+            if (reservation == null)
+            {
+                return NotFound();
+            }
+
+            return reservation;
+        }
+
+        public async Task<ActionResult> RegisterAsync(RegisterModel model)
+        {
+            var result = await _userService.RegisterAsync(model);
+            return Ok(result);
+        }
+
+        // GET: api/Reservations/Available
+        [HttpGet]
+        public async Task<ActionResult<List<Reservation>>> GetAvailableReservations([FromQuery] DateTime start, [FromQuery] DateTime end)
+        {
+            List<Reservation> listReservations = new List<Reservation>();
+
+            Park1APIService x = new Park1APIService();
+            var list1 = await x.GetAvailable(start, end);
+            listReservations.AddRange(list1);
+
+            Park2APIService y = new Park2APIService();
+            var list2 = await y.GetAvailable(start, end);
+            listReservations.AddRange(list2);
+
+
+            return Ok(listReservations);
+
+        }
+
+
+        // PUT: api/Reservations/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutReservation(int id, Reservation reservation)
+        {
+            if (id != reservation.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(reservation).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ReservationExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Reservations
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Reservation>> PostReservation(Reservation reservation)
+        {
+            _context.Reservations.Add(reservation);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetReservation", new { id = reservation.Id }, reservation);
+        }
+
+        // DELETE: api/Reservations/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteReservation(int id)
+        {
+            var reservation = await _context.Reservations.FindAsync(id);
+            if (reservation == null)
+            {
+                return NotFound();
+            }
+
+            _context.Reservations.Remove(reservation);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ReservationExists(int id)
+        {
+            return _context.Reservations.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ iParkPro/iParkPro/Services/Park2APIService.cs
@@ -0,0 +1,17 @@
+﻿using iParkPro.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace iParkPro.Services
+{
+    public class Park2APIService : IParkService
+    {
+        public async Task<List<Reservation>> GetAvailable(DateTime start, DateTime end)
+        {
+            List<Reservation> list = new List<Reservation>();
+            return list;
+        }
+    }
+}
