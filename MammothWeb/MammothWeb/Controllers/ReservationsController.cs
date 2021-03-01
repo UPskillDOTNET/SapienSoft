3efa14421b1d9dbb590a456555f69138f28fa590
João Oliveira <joaoamoliveira8@upskill.pt>
2021-03-01 14:52:21 +0000

    Reservation instanciated

---
+++ MammothWeb/MammothWeb/Controllers/ReservationsController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MammothWeb.Controllers
+{
+    public class ReservationsController : Controller
+    {
+        // GET: ReservationsController
+        public ActionResult Index()
+        {
+            try
+            {
+                return View();
+            }
+            catch
+            {
+                TempData["message"] = "Server connection failed";
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        // GET: ReservationsController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ReservationsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ReservationsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ReservationsController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ReservationsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ReservationsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ReservationsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ MammothWeb/MammothWeb/Models/Reservation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MammothWeb.Models
+{
+    public class Reservation
+    {
+        public DateTime DateCreated { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public string ParkName { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public double Value { get; set; }
+        public string Locator { get; set; }
+        public int SlotId { get; set; }
+        public string UserId { get; set; }
+    }
+}
