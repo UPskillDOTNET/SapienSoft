cb6a5b04ee9926b5e30372518666732c8385da5b
João Oliveira <joaoamoliveira8@upskill.pt>
2021-03-01 15:00:59 +0000

    admin view: reservations list added

--- MammothWeb/MammothWeb/Controllers/ReservationsController.cs
+++ MammothWeb/MammothWeb/Controllers/ReservationsController.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using MammothWeb.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace MammothWeb.Controllers
@@ -14,7 +16,28 @@ namespace MammothWeb.Controllers
         {
             try
             {
-                return View();
+                IEnumerable<Reservation> reservation = null;
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri("https://localhost:44398/api/");
+                    var response = client.GetAsync("reservations");
+                    response.Wait();
+
+                    var result = response.Result;
+                    if (result.IsSuccessStatusCode)
+                    {
+                        var read = result.Content.ReadAsAsync<IList<Reservation>>();
+                        read.Wait();
+                        reservation = read.Result;
+                    }
+                    else
+                    {
+                        //erro
+                        reservation = Enumerable.Empty<Reservation>();
+                        ModelState.AddModelError(string.Empty, "Server error occured");
+                    }
+                    return View(reservation);
+                }
             }
             catch
             {
