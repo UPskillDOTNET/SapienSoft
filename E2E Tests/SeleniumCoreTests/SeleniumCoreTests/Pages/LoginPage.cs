50a6c106171b77ed8a33a384689e9fa1f4f6aa0b
João Oliveira <joaoamoliveira8@upskill.pt>
2021-03-06 00:16:12 +0000

    E2E Tests with Selenium added

---
+++ E2E Tests/SeleniumCoreTests/SeleniumCoreTests/Pages/LoginPage.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeleniumCoreTests.Pages
+{
+    class LoginPage
+    {
+        public LoginPage(IWebDriver webDriver)
+        {
+            Driver = webDriver;
+        }
+
+        public IWebDriver Driver { get; }
+        IWebElement txtEmail => Driver.FindElement(By.Name("Email"));
+        IWebElement txtPassword => Driver.FindElement(By.Name("Password"));
+        IWebElement btnLogin => Driver.FindElement(By.XPath("//input[@value='Submit']"));
+
+        public void Login(string email, string password)
+        {
+            txtEmail.SendKeys(email);
+            txtPassword.SendKeys(password);
+            btnLogin.Submit();
+        }
+    }
+}
