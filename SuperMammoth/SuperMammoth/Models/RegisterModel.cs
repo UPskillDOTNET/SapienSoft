68491a9dc45b673dbd7d7b6a1b0ce76ea1e88256
jorge-mrh <jorge.mrh@upskill.pt>
2021-03-10 15:22:01 +0000

    show password fixes, added it to registration.

--- SuperMammoth/SuperMammoth/Models/RegisterModel.cs
+++ SuperMammoth/SuperMammoth/Models/RegisterModel.cs
@@ -17,6 +17,7 @@ namespace SuperMammoth.Models
         [Required]
         public string Email { get; set; }
         [Required]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
         [Required]
         public int PaymentMethodId { get; set; }
--- SuperMammoth/SuperMammoth/wwwroot/js/sitejs.js
+++
@@ -1,9 +0,0 @@
-﻿$(document).ready(function () {
-    $("#btnShow").mousedown(function () {
-        $("#Password").attr("type", "text");
-    });
-
-    $("#btnShow").on("mouseleave", function () {
-        $("#Password").attr("type", "password");
-    });
-}); 
