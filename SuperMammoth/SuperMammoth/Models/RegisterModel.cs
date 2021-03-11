1e4c2d18ad798e6aefa30110762b4eee74b6c71d
Leandro Costa <leandro.dmc.95@upskill.pt>
2021-03-11 13:40:32 +0000

    minor fixes

--- SuperMammoth/SuperMammoth/Models/RegisterModel.cs
+++ SuperMammoth/SuperMammoth/Models/RegisterModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -21,5 +22,7 @@ namespace SuperMammoth.Models
         public string Password { get; set; }
         [Required]
         public int PaymentMethodId { get; set; }
+        [ForeignKey("PaymentMethodId")]
+        public PaymentMethod PaymentMethod{ get; set; }
     }
 }
