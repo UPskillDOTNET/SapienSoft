884fb1445483d7c98dc8a92fd29326cda8c44a6b
Leandro Costa <leandro.dmc.95@upskill.pt>
2021-03-15 03:41:17 +0000

    Dashboard feito

---
+++ SuperMammoth/SuperMammoth/Models/AreaDataPoints.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace SuperMammoth.Models
+{
+	//DataContract for Serializing Data - required to serve in JSON format
+	[DataContract]
+	public class AreaDataPoints
+	{
+		public AreaDataPoints(int x, double y)
+		{
+			this.X = x;
+			this.Y = y;
+			
+		}
+		//Trying to add a label atribute
+		[DataMember(Name = "x")]
+		public Nullable<int> X = null;
+
+
+        //Explicitly setting the name to be used while serializing to JSON.
+        [DataMember(Name = "y")]
+		public Nullable<double> Y = null;
+
+		
+	}
+}
+
